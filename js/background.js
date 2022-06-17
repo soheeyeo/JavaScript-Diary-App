fb3de3bb28bf1c4cccfe2efddbcfc78737a258d3
Sohee Yeo <95061372+soheeyeo@users.noreply.github.com>
2022-06-18 00:47:19 +0900

    Change Arr

--- js/background.js
+++ js/background.js
@@ -3,14 +3,14 @@ const images = [
     "2.jpg",
     "3.jpg",
     "4.jpg",
-    "5.jpg"
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
 ]
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
-
-console.log(bgImage);
 
 document.body.appendChild(bgImage);
