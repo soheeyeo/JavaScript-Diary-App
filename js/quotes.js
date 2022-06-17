e0bf80e543c434c952e04f237e8c0ddd28db76f5
Sohee Yeo <95061372+soheeyeo@users.noreply.github.com>
2022-06-18 00:44:42 +0900

    Add innerText

--- js/quotes.js
+++ js/quotes.js
@@ -80,10 +80,11 @@ const quotes = [
         author: "Rabindranath Tagore",
     }
 ]
+
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;
+author.innerText = `- ${todaysQuote.author}`;
